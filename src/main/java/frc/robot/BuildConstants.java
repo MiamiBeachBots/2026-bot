950d4a8f91ad20aebeaeee4ef371032124309107
Thalia Webb <thaliathenerder@gmail.com>
2026-03-02 15:55:54 -0500

    chore: Update BuildConstants spotless formatting

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-bot-fork";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 152;
-  public static final String GIT_SHA = "ae3a1c6fbc684eb30d074523ce863fb311d15e9f";
-  public static final String GIT_DATE = "2026-02-25 08:47:01 EST";
+  public static final int GIT_REVISION = 161;
+  public static final String GIT_SHA = "0fd9ab7f26480700bee6eac45c29b249e01a4de3";
+  public static final String GIT_DATE = "2026-03-01 20:47:52 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-02-25 08:49:05 EST";
-  public static final long BUILD_UNIX_TIME = 1772027345373L;
+  public static final String BUILD_DATE = "2026-03-02 15:41:21 EST";
+  public static final long BUILD_UNIX_TIME = 1772484081982L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
