9e2e7330b16ab52466f0810a80b0adf010ba3551
Thalia Webb <Thaliathenerder@gmail.com>
2026-03-18 22:03:02 -0400

    feat: complete overhaul of control systems and ui

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-bot-fork";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 220;
-  public static final String GIT_SHA = "85394b75cf2ef4a4d2e216d359e79cfac49ecc99";
-  public static final String GIT_DATE = "2026-03-10 16:12:30 EDT";
+  public static final int GIT_REVISION = 237;
+  public static final String GIT_SHA = "2ed177d08c4524e63ec937d2588d661f328819ac";
+  public static final String GIT_DATE = "2026-03-18 15:56:54 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-03-11 16:53:56 EDT";
-  public static final long BUILD_UNIX_TIME = 1773262436327L;
+  public static final String BUILD_DATE = "2026-03-18 16:45:13 EDT";
+  public static final long BUILD_UNIX_TIME = 1773866713753L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
